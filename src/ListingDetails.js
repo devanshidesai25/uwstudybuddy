4a06ef624baf33871a58796d57413393dc71eee7
devanshidesai <ddesai22@uw.edu>
2024-03-11 18:01:46 -0700

    finished view full listing

--- src/ListingDetails.js
+++ src/ListingDetails.js
@@ -1,49 +1,48 @@
 import React, { useEffect, useState} from 'react';
 import { getDatabase, ref, get } from 'firebase/database';
 import { useParams } from 'react-router-dom';
+import { getStorage, ref as storageRef, getDownloadURL } from 'firebase/storage';
+
 
 function ListingDetails() {
-  const [listingData, setListingData] = useState(null);
+  const [listingData, setListingData] = useState();
   const { id } = useParams();
 
   useEffect(() => {
     const fetchData = async () => {
-      try {
-        const database = getDatabase();
-        const snapshot = await get(ref(database, `supplyListings/${id}`));
-
-        if (snapshot.exists()) {
-          const data = snapshot.val();
-          setListingData(data);
-        } else {
-          console.log('No data available');
-        }
-      } catch (error) {
-        console.error('Error fetching data:', error);
+      const database = getDatabase();
+      const snapshot = await get(ref(database, `supplyListings/${id}`));
+      if (snapshot.exists()) {
+        const data = snapshot.val();
+        setListingData(data);
+      } else {
+        console.log('No data available');
       }
     };
     fetchData();
   }, [id]);
 
-  const contactOwner = () => {
-    const ownerEmail = listingData ? listingData.email : '';
-    window.location.href = `mailto:${ownerEmail}`;
+  const contactSeller = () => {
+    const sellerEmail = listingData.email;
+    window.location.href = `mailto:${sellerEmail}`;
   };
 
+
   return (
-    <div className="listing-info">
+    <div className="listing-container">
       {listingData ? (
         <>
-          <p>{listingData.name}</p>
-          <p>{listingData.condition}</p>
-          <p>{listingData.price}</p>
-          <p>{listingData.description}</p>
-          <button className="contact-owner-button" onClick={contactOwner}>
-            Contact Owner
+          <img src={listingData.image}></img>
+          <h3>{listingData.name}</h3>
+          <p>Condition: {listingData.condition}</p>
+          <p>Price: ${listingData.price}</p>
+          <p>Note from seller: {listingData.description}</p>
+          <button className="contact-seller-button" onClick={contactSeller}>
+            Contact Seller
           </button>
         </>
       ) : (
-        <p>Loading...</p>
+        <p>Error Retrieving Data!</p>
       )}
     </div>
   );
