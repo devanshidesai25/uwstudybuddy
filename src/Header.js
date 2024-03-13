45408a30533c3ff9858b3ae0fc5f0b3cc8c5bd17
TheaKlein <sklein3@uw.edu>
2024-03-12 19:36:19 -0700

    dropdown

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom'; 
+import studyBuddyLogo from './img/studybuddy-logo.png';
+
+function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  return (
+    <div className="menu-bar"> 
+      <div className="logo-container">
+        <Link to="/"><img src={studyBuddyLogo} alt="Study Buddy Logo" className="logo" /></Link>
+      </div>
+      <div id="hamburger-menu" onClick={toggleMenu}>
+        <a href="#"><i className="fa fa-bars" aria-label="menu"></i></a>
+      </div>
+      <ul className={showMenu ? "show" : ""}>
+        <li><Link to="/profile">Profile</Link></li> 
+        <li><Link to="/friends">Friends</Link></li> 
+        <li><Link to="/shop">Shop</Link></li> 
+        <li><Link to="/sell">Sell</Link></li> 
+        <li><Link to="/favorites">Favorites</Link></li>
+        <li><Link to="/events">Events</Link></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Header;
+
+
+
+/*import React from 'react';
 import { Link } from 'react-router-dom'; 
 import studyBuddyLogo from './img/studybuddy-logo.png';
 
 function Header() {
   return (
     <div className="menu-bar"> 
+      <div className="logo-container">
+          <Link to="/"><img src={studyBuddyLogo} alt="Study Buddy Logo" className="logo" /></Link>
+      </div>
       <ul>
-        <li><Link to="/"><img src={studyBuddyLogo} alt="Study Buddy Logo" className="logo" /></Link></li>
         <li><Link to="/profile">Profile</Link></li> 
         <li><Link to="/friends">Friends</Link></li> 
         <li><Link to="/shop">Shop</Link></li> 
@@ -18,4 +55,4 @@ function Header() {
   );
 }
 
-export default Header;
+export default Header; */
