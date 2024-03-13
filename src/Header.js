566eee89824539b01d0b9fca08f7d549a2ff1516
devanshidesai <ddesai22@uw.edu>
2024-03-12 20:04:11 -0700

    fixed header and impelemented hamburger menu

--- src/Header.js
+++ src/Header.js
@@ -14,9 +14,11 @@ function Header() {
       <div className="logo-container">
         <Link to="/"><img src={studyBuddyLogo} alt="Study Buddy Logo" className="logo" /></Link>
       </div>
-      <div id="hamburger-menu" onClick={toggleMenu}>
-        <a href="#"><i className="fa fa-bars" aria-label="menu"></i></a>
-      </div>
+      {!showMenu && (
+        <div id="hamburger-menu" onClick={toggleMenu}>
+          <a href="#"><i className="fa fa-bars" aria-label="menu"></i></a>
+        </div>
+      )}
       <ul className={showMenu ? "show" : ""}>
         <li><Link to="/profile">Profile</Link></li> 
         <li><Link to="/friends">Friends</Link></li> 
@@ -30,29 +32,3 @@ function Header() {
 }
 
 export default Header;
-
-
-
-/*import React from 'react';
-import { Link } from 'react-router-dom'; 
-import studyBuddyLogo from './img/studybuddy-logo.png';
-
-function Header() {
-  return (
-    <div className="menu-bar"> 
-      <div className="logo-container">
-          <Link to="/"><img src={studyBuddyLogo} alt="Study Buddy Logo" className="logo" /></Link>
-      </div>
-      <ul>
-        <li><Link to="/profile">Profile</Link></li> 
-        <li><Link to="/friends">Friends</Link></li> 
-        <li><Link to="/shop">Shop</Link></li> 
-        <li><Link to="/sell">Sell</Link></li> 
-        <li><Link to= "/favorites">Favorites</Link></li>
-        <li><Link to= "/events">Events</Link></li>
-      </ul>
-    </div>
-  );
-}
-
-export default Header; */
